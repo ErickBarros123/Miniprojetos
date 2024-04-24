737125af0c844c1050a79c44032b823a231939b2
erick <erickbarrosdasilva123456@gmail.com>
2024-04-23 22:17:43 -0300

    Miniprojeto2

--- MiniProjeto2/src/MiniProjeto2.java
+++ MiniProjeto2/src/MiniProjeto2.java
@@ -7,18 +7,23 @@ public class MiniProjeto2 {
       
         String mac = "machado";
         String arm = "arma";
-        String mascara = "mascaragas";
+        String mascara = "mascaradegas";
         String venen = "veneno";
         String adicionar;
+        
+        Scanner ad = new Scanner(System.in);
 
+        System.out.println("Digite o nome do seu: \n");
+        String nomepersonagem = ad.nextLine(); // Lê o nome do personagem
        
         
-        System.out.println("Lucas está dentro de um abismo, ele precisa sair desse abismo através de duas\n" +
-                           " passagens que estão logo à sua frente. Mas algo irá impedir Lucas. Em cada passagem\n" + 
-                           "há um obstáculo a ser vencido. No começo do abismo, onde Lucas está, há uma caixa de instrumentos.\n" + 
-                           "Certifique-se de escolher bem os instrumentos que garantirão a sobrevivência de Lucas.\n"); 
+        System.out.println(nomepersonagem + ", voce se encontra dentro de um abismo, você precisa sair desse abismo através de duas\n" +
+                           " passagens que estão logo à sua frente. Mas algo irá tentar lhe impedir. Em cada passagem\n" + 
+                           "há um obstáculo a ser vencido. No começo do abismo, onde vc está, há uma caixa de instrumentos.\n" + 
+                           "Certifique-se de escolher bem os instrumentos que garantirão sua sobrevivência .\n" +
+                           "instrumentos são: machado, veneno, arma e mascaradegas"); 
 
-        Scanner ad = new Scanner(System.in);
+        
 
         System.out.println("Digite o primeiro instrumento: \n");
         String primeiroInstrumento = ad.nextLine(); // Lê o primeiro instrumento
@@ -26,8 +31,10 @@ public class MiniProjeto2 {
         System.out.println("Digite o segundo instrumento: \n");
         String segundoInstrumento = ad.nextLine(); // Lê o segundo instrumento
 
-        System.out.println("Muito bem. Espero que tenha escolhido bem seus instrumentos, eles serão decisivos para a vida ou morte do personagem.\n");
-        
+        System.out.println("Muito bem. Espero que tenha escolhido bem seus instrumentos, eles serão decisivos para a vida ou morte do personagem.\n" + "Aparte  1 para prosseguir");
+        int prosseguir = ad.nextInt();
+        System.out.println(" O monstro apareceu, voce precisa derrotá-lo");
+    
         if (primeiroInstrumento.equals(arm) || segundoInstrumento.equals(venen)) {
             while(true) {
                 System.out.println("1 para atirar com a arma" + "\n" + "2 tomar elixir para recuperar vida" + "\n" + "3 para jogar veneno no monstro" );
@@ -35,15 +42,15 @@ public class MiniProjeto2 {
 
                 if (adicionar.equals("1")) {
                     System.out.println("O dano da arma foi de: " + personagem.ataque + "\n");
-                    System.out.println(" Voce atirou no monstro. A vida do monstro é:  " + (monstro.hp -= personagem.ataque) );
+                    System.out.println(nomepersonagem + " Voce atirou no monstro. A vida do monstro foi para:  " + (monstro.hp -= personagem.ataque) );
                     System.out.println("O monstro contra atacou, o dano do monstro foi de:  " + monstro.dano);
-                    System.out.println("sua vida foi para:  " + (personagem.vida -= monstro.dano));
+                    System.out.println(nomepersonagem + " sua vida foi para:  " + (personagem.vida -= monstro.dano));
                     System.out.println("----------------------------------------------------------------------");
                     if (monstro.hp <= 0){
-                      System.out.println("Lucas conseguiu matar o monstro. Mas ainda falta um obstáculo, O Guardiao da porta seguinte lhe espera para um combate ainda mais insano");
+                      System.out.println(nomepersonagem + " conseguiu matar o monstro. Mas ainda falta um obstáculo, O Guardiao da porta seguinte lhe espera para um combate ainda mais insano");
                       break;}
                     if (personagem.vida <=0){
-                    System.out.println(" Lucas morreu.");
+                    System.out.println(nomepersonagem + " morreu.");
                       break;
                     
 
@@ -51,20 +58,21 @@ public class MiniProjeto2 {
                 } 
                else if (adicionar.equals("2")){
 
-                System.out.println(" sua vida foi para: " + (personagem.vida += personagem.elixir));
+                System.out.println(nomepersonagem  + " sua vida foi para: " + (personagem.vida += personagem.elixir));
                }
 
                else if (adicionar.equals("3")){
-                  System.out.println("O dano da arma foi de: " + personagem.veneno + "\n");
+                  System.out.println("O dano do veneno foi de: " + personagem.veneno + "\n");
                   System.out.println("A vida do monstro foi para:  " + (monstro.hp -= personagem.veneno) );
-                  System.out.println("O monstro contra atacou, o dono do monstro foi de:  " + monstro.dano);
-                  System.out.println("sua vida foi para:  " + (personagem.vida -= monstro.dano));
-                  System.out.println("----------------------------------------------------------------------");
-                 if (monstro.hp <= 0){
-                    System.out.println("Lucas conseguiu matar o monstro. Mas ainda falta um obstáculo, O Guardiao da porta seguinte lhe espera para um combate ainda mais insano");
+                  if (monstro.hp <= 0){
+                    System.out.println(nomepersonagem + " conseguiu matar o monstro. Mas ainda falta um obstáculo, O Guardiao da porta seguinte lhe espera para um combate ainda mais insano");
                       break;}
+                  System.out.println("O monstro contra atacou, o dono do monstro foi de:  " + monstro.dano);
+                  System.out.println(nomepersonagem + " sua vida foi para:  " + (personagem.vida -= monstro.dano));
+                  System.out.println("----------------------------------------------------------------------");
+                 
                  if (personagem.vida <=0){
-                     System.out.println("Lucas morreu.");
+                     System.out.println(nomepersonagem + " morreu.");
                       break;
                       
                }
@@ -73,7 +81,7 @@ public class MiniProjeto2 {
         }
         
         else {
-            System.out.println("Lucas morreu para o monstro\n");
+            System.out.println(nomepersonagem + " morreu para o monstro\n");
         }
         
         ad.close(); // Fechando o scanner
--- MiniProjeto2/src/Personagem.java
+++ MiniProjeto2/src/Personagem.java
@@ -2,7 +2,7 @@
 import java.util.Random;
 public class Personagem {
 
-     
+    
     public int ataque;
     public int elixir;
     public int vida;
